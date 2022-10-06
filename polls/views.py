4b8274ad498d7e75577e31cf42003440fbe86c4b
Sam Larson <samlarson94@gmail.com>
2022-10-06 10:14:10 -0500

    Adding functionality to vote view.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, HttpResponse
-from .models import Question
+from django.shortcuts import HttpResponseRedirect, render, HttpResponse, get_object_or_404
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -21,4 +22,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
