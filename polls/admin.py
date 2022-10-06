ef147f55beb3bdd976cccf7a50ab8133244558e7
Sam Larson <samlarson94@gmail.com>
2022-10-06 15:20:13 -0500

    Adding mock background image to CSS. Making adjustments to Django admin Question view, adding choices.

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Information', {'fields': ['question_text']}), 
+        ('Date & Time Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
